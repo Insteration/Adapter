d01d8118053ce8dc9dea8e291c4b275796388596
Artem Karmaz <inste@icloud.com>
2019-04-17 12:01:16 +0300

    Initial Commit

---
+++ Adapter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adapter
+//
+//  Created by Artem Karmaz on 4/17/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
